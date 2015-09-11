12bc3caa2e3b118e4640e80ae750511234c28335
Kari Moore <moorekari9@gmail.com>
2015-09-11 12:34:10 -0500

    Assignment 14 Best Practices Complete

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -6,36 +6,43 @@ import java.io.InputStreamReader;
 
 public class WorstPractices extends Object {
 
-	/************************************************
-	 *	ASSIGNMENT:
-	 *	Fix code and comments below this block
-	/************************************************/
+    /************************************************
+     *	ASSIGNMENT:
+     *	Fix code and comments below this block
+    /************************************************/
+    public static final int CONST_NUMBER = 5;
+    public static final int CONST_COUNT = 18;
+        
+    public static void main(String [] args) {
+        int magicNumber = WorstPractices.animalEquation(false);
+        magicNumber *= CONST_NUMBER;
+        if (magicNumber > CONST_COUNT) {
+            while(magicNumber > 0) {
+                magicNumber--;
+            }
+        }
+    }
 
-	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
-	}
+    /***************************************
+     *   Class:         WorstPractice
+     *   Method:        animalEquation
+     *   Param:         isAnimal
+     *   Return:        returnNumber 
+     *   Description:   If isAnimal TRUE return the number as a 
+     *                  result of using ANIMAL_NUMBER, Otherwise 
+     *                  return the solution using NOT_AN_ANIMAL_NUMBER
+     *****************************************/
+    private static int animalEquation(boolean isAnimal) {
+        final int ANIMAL_NUMBER = 34;
+        final int NOT_AN_ANIMAL_NUMBER = 21;
+        float returnNumber = 0.5f;
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
-}
+        int animalNumber = isAnimal ? ANIMAL_NUMBER : NOT_AN_ANIMAL_NUMBER;
+        for (int i = 0; i <= animalNumber; i++) { 
+            returnNumber *= animalNumber;
+        } 
+        return (int) returnNumber;
+    }
 
 	/************************************************
 	 *	ASSIGNMENT
