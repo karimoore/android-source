a711a748d5dace438a77ff60324389743a3f2761
Kari Moore <moorekari9@gmail.com>
2015-09-23 13:12:03 -0500

    Assignment 24 Complete

--- foundation/24-interfaces/src/com/bloc/interfaces/people/hobbies/Driver.java
+++ foundation/24-interfaces/src/com/bloc/interfaces/people/hobbies/Driver.java
@@ -13,7 +13,7 @@ public interface Driver {
 	 *
 	 * @return nothing
 	 */
-
+         public void getInCar();
 	/*
 	 * startEngine
 	 *
@@ -21,6 +21,7 @@ public interface Driver {
 	 *
 	 * @return nothing
 	 */
+         public void startEngine();
 
 	/*
 	 * driveFast
@@ -29,4 +30,5 @@ public interface Driver {
 	 *
 	 * @return nothing
 	 */
+         public void driveFast();
 }
--- foundation/24-interfaces/src/com/bloc/interfaces/people/hobbies/Skydiver.java
+++ foundation/24-interfaces/src/com/bloc/interfaces/people/hobbies/Skydiver.java
@@ -13,7 +13,7 @@ public interface Skydiver {
 	 *
 	 * @return nothing
 	 */
-
+         public void getInPlane();
 	/*
 	 * jumpFromPlane
 	 *
@@ -21,6 +21,7 @@ public interface Skydiver {
 	 *
 	 * @return nothing
 	 */
+         public void jumpFromPlane();
 
 	/*
 	 * releaseParachute
@@ -29,4 +30,5 @@ public interface Skydiver {
 	 *
 	 * @return nothing
 	 */
+         public void releaseParachute();
 }
