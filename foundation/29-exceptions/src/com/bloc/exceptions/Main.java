fa7b8b0fb4ce691e3d59c1c5f26d8c0cbe86df07
Kari Moore <moorekari9@gmail.com>
2015-09-30 11:16:56 -0500

    Assignment 29 Complete

--- foundation/29-exceptions/src/com/bloc/exceptions/Main.java
+++ foundation/29-exceptions/src/com/bloc/exceptions/Main.java
@@ -33,16 +33,23 @@ public class Main extends Object {
 	/************************************************/
 	private static final void tryGetMax() {
 		int max = 0;
-		max = FunMethods.getMax((Integer[])null);
-		Integer[] numbers = new Integer[50];
-		Random rand = new Random();
-		for (int i = 0; i < 50; i++) {
-			numbers[i] = new Integer(rand.nextInt(500));
-		}
+                Random rand = new Random();
+                Integer[] numbers = new Integer[50];
+                try {
+                    max = FunMethods.getMax((Integer[])null);
+                } catch (IllegalArgumentException e){
+                    //fix empty array
+                    for (int i = 0; i < 50; i++) {
+                            numbers[i] = new Integer(rand.nextInt(500));
+                    }                
+                }
 		numbers[32] = null;
-		max = FunMethods.getMax(numbers);
-		numbers[32] = new Integer(rand.nextInt(500));
-		max = FunMethods.getMax(numbers);
+                try {
+                    max = FunMethods.getMax(numbers);
+                } catch (IllegalStateException e){
+                    numbers[32] = new Integer(rand.nextInt(500));
+                    max = FunMethods.getMax(numbers);               
+                }
 	}
 
 	/************************************************
@@ -50,16 +57,23 @@ public class Main extends Object {
  	 *	Catch thrown exceptions
 	/************************************************/
 	private static final void tryRemove() {
+            Object[] someObjects = new Object[12];
+            try{
 		FunMethods.remove(null, 2);
-		Object[] someObjects = new Object[12];
-		someObjects[0] = "a string!";
-		someObjects[1] = new Integer(32);
-		someObjects[2] = new Float(42.5f);
-		someObjects[3] = "another string";
-		for (int i = 4; i < someObjects.length; i++) {
-			someObjects[i] = String.valueOf(i);
-		}
+            } catch (IllegalArgumentException e){
+                // fill in array
+                someObjects[0] = "a string!";
+                someObjects[1] = new Integer(32);
+                someObjects[2] = new Float(42.5f);
+                someObjects[3] = "another string";              
+            }
+            for (int i = 4; i < someObjects.length; i++) {
+            	someObjects[i] = String.valueOf(i);
+            }
+            try {
 		FunMethods.remove(someObjects, 12);
+            } catch (IndexOutOfBoundsException e){
 		someObjects = FunMethods.remove(someObjects, 3);
+            }
 	}
 }
