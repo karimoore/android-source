12029cb8cf9a23dee448e1f180cb362e3956a9a0
Kari Moore <moorekari9@gmail.com>
2015-10-01 14:42:22 -0500

    Assignment 31 Complete

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,5 +1,11 @@
 package com.bloc.threads;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
 public class ImageGetter extends Thread {
 
 	/*
@@ -18,7 +24,12 @@ public class ImageGetter extends Thread {
  	 *	The variables passed into it must impact the `run()` method's
  	 *	behavior as described above.
 	/************************************************/
-
+        private String mUrl;
+        private boolean mOpenWhenCompleted;
+        public ImageGetter(String url, boolean openWhenCompleted){
+            mUrl = url;
+            mOpenWhenCompleted = openWhenCompleted;
+        }
 	@Override
 	public void run() {
 		/************************************************
@@ -26,5 +37,29 @@ public class ImageGetter extends Thread {
  		 *	Perform the work found in `Main.java` in this
  		 *	Thread instead.
 		/************************************************/
+            
+            try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			URL url = new URL(mUrl);
+			BufferedImage bufferedImage = ImageIO.read(url);
+                        File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if (mOpenWhenCompleted) {
+                            if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+                            } else {
+                                    Runtime.getRuntime().exec("mspaint.exe google_logo.png");
+                            }
+                        }
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
