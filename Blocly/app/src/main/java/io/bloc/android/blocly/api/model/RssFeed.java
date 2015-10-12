c67822070e46eb0de730dfe7564d77d8305f0256
Kari Moore <moorekari9@gmail.com>
2015-10-12 13:12:29 -0500

    Checkpoint 35 Complete, dataSource

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/model/RssFeed.java
@@ -0,0 +1,50 @@
+package io.bloc.android.blocly.api.model;
+
+/**
+ * Created by Kari on 10/12/2015.
+ */
+public class RssFeed {
+    private String title;
+    private String description;
+    private String siteUrl;
+    private String feedUrl;
+
+    public RssFeed(String title, String description, String siteUrl, String feedUrl) {
+        this.title = title;
+        this.description = description;
+        this.siteUrl = siteUrl;
+        this.feedUrl = feedUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSiteUrl() {
+        return siteUrl;
+    }
+
+    public void setSiteUrl(String siteUrl) {
+        this.siteUrl = siteUrl;
+    }
+
+    public String getFeedUrl() {
+        return feedUrl;
+    }
+
+    public void setFeedUrl(String feedUrl) {
+        this.feedUrl = feedUrl;
+    }
+}
---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/model/RssItem.java
@@ -0,0 +1,110 @@
+package io.bloc.android.blocly.api.model;
+
+/**
+ * Created by Kari on 10/12/2015.
+ */
+public class RssItem {
+    private String guid;
+    private String title;
+    private String description;
+    private String url;
+    private String imageUrl;
+    private long rssFeedId;
+    private long datePublished;
+    private boolean read;
+    private boolean favorite;
+    private boolean archived;
+
+    public RssItem(String guid, String title, String description, String url, String imageUrl, long rssFeedId, long datePublished, boolean read, boolean favorite, boolean archived) {
+        this.guid = guid;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.imageUrl = imageUrl;
+        this.rssFeedId = rssFeedId;
+        this.datePublished = datePublished;
+        this.read = read;
+        this.favorite = favorite;
+        this.archived = archived;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public long getRssFeedId() {
+        return rssFeedId;
+    }
+
+    public void setRssFeedId(long rssFeedId) {
+        this.rssFeedId = rssFeedId;
+    }
+
+    public long getDatePublished() {
+        return datePublished;
+    }
+
+    public void setDatePublished(long datePublished) {
+        this.datePublished = datePublished;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+}
