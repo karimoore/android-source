e23a820697b028867f8ca7358f34f67c60ff0b56
Kari Moore <moorekari9@gmail.com>
2015-11-22 22:46:24 -0600

    Checkpoint 59 Complete

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -28,6 +28,7 @@ import io.bloc.android.blocly.api.DataSource;
 import io.bloc.android.blocly.api.model.RssFeed;
 import io.bloc.android.blocly.api.model.RssItem;
 import io.bloc.android.blocly.ui.adapter.NavigationDrawerAdapter;
+import io.bloc.android.blocly.ui.fragment.RssItemDetailFragment;
 import io.bloc.android.blocly.ui.fragment.RssItemListFragment;
 
 /**
@@ -46,6 +47,7 @@ public class BloclyActivity extends AppCompatActivity
     private View overflowButton;
     private List<RssFeed> allFeeds = new ArrayList<RssFeed>();
     private RssItem expandedItem = null;
+    private boolean onTablet;
 
 /*
     Private methods
@@ -75,6 +77,7 @@ public class BloclyActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
 
+        onTablet = findViewById(R.id.fl_activity_blocly_right_pane) != null;
         Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
         setSupportActionBar(toolbar);
 
@@ -248,6 +251,12 @@ public class BloclyActivity extends AppCompatActivity
     @Override
     public void onItemExpanded(RssItemListFragment rssItemListFragment, RssItem rssItem) {
         expandedItem = rssItem;
+        if (onTablet) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fl_activity_blocly_right_pane, RssItemDetailFragment.detailFragmentForRssItem(rssItem))
+                    .commit();
+            return;
+        }
         animateShareItem(expandedItem != null);
     }
 
